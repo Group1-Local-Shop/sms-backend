be6416e3c83988486035652ea12e23c3cae9110c
jbdura <bidurajohnson@gmail.com>
2022-10-20 14:47:20 +0300

    suppliers Serializer

--- app/serializers/supplier_serializer.rb
+++ app/serializers/supplier_serializer.rb
@@ -1,3 +1,3 @@
 class SupplierSerializer < ActiveModel::Serializer
-  attributes :id, :name, :product_quantity, :buying_price, :payment_status
+  attributes :id, :name, :product_id, :product_quantity, :buying_price, :payment_status
 end
