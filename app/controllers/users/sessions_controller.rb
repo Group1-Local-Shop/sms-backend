ac49c4b1263a200ee6e6bbd4944236606019bd9c
Gaudencia <conny@students.uonbi.ac.ke>
2022-10-21 21:40:37 +0300

    initial commit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
   private
 
-  def respond_with(resource, _opts = {})
+  def respond_with(resource,options={})
     render json: {
       status: {code: 200, message: 'Logged in sucessfully.'},
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
@@ -12,6 +12,8 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
+    jwt_payload=JWT.decode(request.headers['Authorization'].split(' ')[1],Rails.application.secret_key_base).first
+    current_user=User.find(jwt_payload['sub'])
     if current_user
       render json: {
         status: 200,
