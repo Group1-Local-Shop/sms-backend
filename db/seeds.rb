dcec4db5c4e32bd923b2cb66a78014540f0c9b45
Allan <github email address>
2022-10-29 10:31:03 +0300

    product description

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,63 +34,63 @@ stores.create!([
 
 product.create!([
   {
-    image: "",
+    image: "https://images.unsplash.com/photo-1617571607645-dd7dd3bf7f6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
     product_id: "Circular Saw",
-    description: "",
+    description: "A power-saw using a toothed blade to cut different materials using a rotary motion spinning around an arbor.",
     
   },
   {
-    image: "",
+    image: "https://plus.unsplash.com/premium_photo-1663045230418-b3150d525a12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
     product_id: "Jig Saw",
-    description: "",
+    description: "A power saw which uses a reciprocating blade to cut irregular curves, such as stenciled designs, in wood, metal, or other materials. ",
     
   },
   {
-    image: "",
+    image: "https://images.unsplash.com/photo-1560846389-956694677531?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2F3fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
     product_id: "Table Saw",
-    description: "",
+    description: "A tool, consisting of a circular saw blade, mounted on an arbor, that is driven by an electric motor. The blade protrudes to provide support for the material.",
     
   },
   {
-    image: "",
+    image: "https://images.unsplash.com/photo-1619759247378-6a73e3ad45f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
     product_id: "Panel Saw",
-    description: "",
+    description: "Horizontal large table saw machine with a sliding feed table that pushes the material through the blade. used to cut sheets of materials to size",
     
   },
   {
-    image: "",
-    product_id: "Reciprocating Saw",
-    description: "",
+    image: "https://images.unsplash.com/photo-1581788604067-769a11325b0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+    product_id: "Chain Saw",
+    description: "A portable gasoline-, electric-, or battery-powered saw that cuts with a set of teeth attached to a rotating chain driven along a guide bar.",
     
   },
   {
-    image: "",
+    image: "https://images.unsplash.com/photo-1594320990326-398050e0f31a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80",
     product_id: "Miter Saw",
-    description: "",
+    description: "A portable gasoline-, electric-, or battery-powered saw that cuts with a set of teeth attached to a rotating chain driven along a guide bar.",
     
   },
   {
-    image: "",
+    image: "https://images.unsplash.com/photo-1615974679960-414c1c7f08ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80",
     product_id: "Track Saw",
-    description: "",
+    description: "A circular saw which can plunge into the material to a predetermined depth during the cut. The depth-of-cut is not fixed and can be adjusted.",
     
   },
   {
-    image: "",
+    image: "https://plus.unsplash.com/premium_photo-1661956403492-84f33a9b809d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHNhd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
     product_id: "Chop Saw",
-    description: "",
+    description: "Automated saw for tree de-barking",
     
   },
   {
-    image: "",
-    product_id: "Band Saw",
-    description: "",
+    image: "https://images.unsplash.com/photo-1659147922585-fd07e0d5e9ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    product_id: "Belt Sander",
+    description: "An electric motor that turns a pair of drums on which a continuous loop of sandpaper is mounted.can be used to sand edges of boards",
     
   },
   {
-    image: "",
-    product_id: "Rip Saw",
-    description: "",
+    image: "https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+    product_id: "Impact Drill",
+    description: "A type of rotary drill with an impact mechanism that generates a hammering motion in rapid succession thrusts to pulverize the material to be bored, so as to provide quicker drilling with less effort.",
     
   }
 ])
